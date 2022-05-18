13bd11b33ca9c75b6376447dfb9a66330ddc54c8
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-05-18 23:36:47 +0200

    basic size and styling applied and ready for development

--- public/wp-content/themes/sykos/index.php
+++ public/wp-content/themes/sykos/index.php
@@ -1,47 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Start | sykos</title>
-        
-        <script type="module">
-            document.documentElement.classList.remove('no-js');
-            document.documentElement.classList.add('js');
-        </script>
-    
-        <meta name="description"
-        content="In Zeiten tiefgreifender Veränderungen sind wesentliche Fertigkeiten hilfreich, um Lösungen zu finden und definierte Ziele zu erreichen. " />
-        <meta property="og:title" content="Start | sykos" />
-        <meta property="og:site_name" content="sykos">
-        <meta name="keywords" content="Beratung, Coaching">
-        <meta property="og:description"
-        content="In Zeiten tiefgreifender Veränderungen sind wesentliche Fertigkeiten hilfreich, um Lösungen zu finden und definierte Ziele zu erreichen. " />
-        <meta property="og:image" content="<?php echo get_template_directory_uri() ?>/images/Sykos-banner.png" />
-        <meta property="og:image:alt" content="Logo von Sykos" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="Start | sykos">
-        <meta property="og:url" content="https://www.www.sykos.net" />
-        <link rel="canonical" href="https://www.www.sykos.net" />
-    
-        <link rel="icon" href="<?php echo get_template_directory_uri() ?>/favicons/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/favicons/favicon-16x16.png">
-        <link rel="manifest" href="<?php echo get_template_directory_uri() ?>/favicons/site.webmanifest">
-        <link rel="mask-icon" href="<?php echo get_template_directory_uri() ?>/favicons/safari-pinned-tab.svg" color="#f683a4">
-        <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/favicons/favicon.ico">
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-config" content="<?php echo get_template_directory_uri() ?>/favicons/browserconfig.xml">
-        <meta name="theme-color" content="#ffffff">
-    
-        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/style.css">
-    </head>
+    <?php include 'parts/head.php'; ?>
 <body>
-    <header>
-        <nav>
+    <?php //wp_body_open();?>
+    <header class="hero-home">
+        <nav class="navbar">
             <ul>
                 <li><a href="">Home</a></li>
                 <li><a href="">Philosophie</a></li>
@@ -51,22 +14,20 @@
                 <li><a href="">Kontakt</a></li>
             </ul>
         </nav>
-        <img id="hero-logo" src="<?php echo get_template_directory_uri() ?>/media/SYKOS-logo-RGB-white.png" alt="SYKOS-logo-RGB-white">
-        <cite>Sie können die hohen Wellen des Meeres nicht ändern, so sehr Sie es versuchen. Zielführender ist, zu lernen, wie man in schwierigen Situationen navigiert</cite>
-
+        <img class="hero-home__logo" src="<?php echo get_template_directory_uri() ?>/media/SYKOS-logo-RGB-white.png" alt="SYKOS-logo-RGB-white">
+        <cite class="hero-home__cite">Sie können die hohen Wellen des Meeres nicht ändern, so sehr Sie es versuchen. Zielführender ist, zu lernen, wie man in schwierigen Situationen navigiert</cite>
     </header>
     <main>
-        <section id="section-intro">
+        <section class="section-intro">
             <h2>Herzlich willkommen in der Welt von SYKOS</h2>
             <p>Ihre Projekte sind einer unruhigen See mit unerwartet hohen Wellen ausgesetzt? <br>In Zeiten tiefgreifender Veränderungen sind wesentliche Fertigkeiten hilfreich, um Lösungen zu finden und definierte Ziele zu erreichen.</p>
         </section>
-        <section id="section-mission">
-            <h2>Herzlich willkommen in der Welt von SYKOS</h2>
-            <p>Ihre Projekte sind einer unruhigen See mit unerwartet hohen Wellen ausgesetzt? <br>In Zeiten tiefgreifender Veränderungen sind wesentliche Fertigkeiten hilfreich, um Lösungen zu finden und definierte Ziele zu erreichen.</p>
+        <section class="section-mission">
+            <h3>Unsere Mission</h3>
+            <p>Wir begleiten Sie auf Ihrem Weg, und Sie bleiben auf Kurs. SYKOS ist zur Stelle, um in bewegten Zeiten anzupacken, wenn sich Hindernisse in den Weg stellen. Wir trainieren Sie, damit Sie auch unter Druck erfolgreich performen.</p>
         </section>
     </main>
-    <footer>
-
-    </footer>
+    <?php include 'parts/footer.php'; ?>
 </body>
+<script src="<?php echo get_template_directory_uri().'/js/script.js' ?>"></script>
 </html>
---
+++ src/php/parts/head.php
@@ -0,0 +1,39 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start | sykos</title>
+    
+    <script type="module">
+        document.documentElement.classList.remove('no-js');
+        document.documentElement.classList.add('js');
+    </script>
+
+    <meta name="description"
+    content="In Zeiten tiefgreifender Veränderungen sind wesentliche Fertigkeiten hilfreich, um Lösungen zu finden und definierte Ziele zu erreichen. " />
+    <meta property="og:title" content="Start | sykos" />
+    <meta property="og:site_name" content="sykos">
+    <meta name="keywords" content="Beratung, Coaching">
+    <meta property="og:description"
+    content="In Zeiten tiefgreifender Veränderungen sind wesentliche Fertigkeiten hilfreich, um Lösungen zu finden und definierte Ziele zu erreichen. " />
+    <meta property="og:image" content="<?php echo get_template_directory_uri() ?>/images/Sykos-banner.png" />
+    <meta property="og:image:alt" content="Logo von Sykos" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Start | sykos">
+    <meta property="og:url" content="https://www.www.sykos.net" />
+    <link rel="canonical" href="https://www.www.sykos.net" />
+
+    <link rel="icon" href="<?php echo get_template_directory_uri() ?>/favicons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/favicons/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri() ?>/favicons/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri() ?>/favicons/safari-pinned-tab.svg" color="#f683a4">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="msapplication-config" content="<?php echo get_template_directory_uri() ?>/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/style.css">
+</head>
