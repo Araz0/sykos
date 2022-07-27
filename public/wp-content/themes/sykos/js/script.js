d369f37a1ea7a8c2366d009599f206342e35890f
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-07-27 22:40:56 +0200

    fix: bug with undefind slides handled

--- public/wp-content/themes/sykos/js/script.js
+++ public/wp-content/themes/sykos/js/script.js
@@ -71,11 +71,13 @@ showSlides();
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("sp-testimonial-item");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+    if (slides.length > 0) {
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 5000); // Change image every 5 seconds
+    }    
 } 
